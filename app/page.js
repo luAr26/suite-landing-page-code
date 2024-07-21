2757251783ef613d567b1557edd8e916fb175af4
Raul <raul_cosmin_26@yahoo.com>
2024-07-21 09:37:18 +0300

    Convert project to Next.js

---
+++ app/page.js
@@ -0,0 +1,14 @@
+/** @format */
+
+export const metadata = {
+  title: "Frontend Mentor | Suite landing page",
+};
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello Nextjs</h1>
+      <div></div>
+    </>
+  );
+}
