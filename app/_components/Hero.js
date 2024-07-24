9ea44d4f14f3e74b9910db08f47e0b319b688bae
Raul <raul_cosmin_26@yahoo.com>
2024-07-24 22:29:55 +0300

    Make some small progress.

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -5,30 +5,35 @@ import Image from "next/image";
 import Stats from "./Stats";
 
 function Hero() {
+  // Example background position: bg-[right_13vw_top_20%]
   return (
-    <section>
-      <h1 className='pt-9 pr-3 text-[38px] leading-[44px] bg-[url(/pattern-curved-line-1.svg)] bg-no-repeat bg-right-top bg-[length:151px_19px] mt-14 mb-8'>
-        A <span className='font-bold'>super solution</span>
-        <br /> for your <span className='font-bold'>business</span>.
-      </h1>
-      <p className='text-grey text-[1rem] leading-[28px] tracking-[0.09px] mb-8'>
-        Our marketing and sales automation help you scale your outreach to get
-        more leads for your company.
-      </p>
-      <p className='mb-14'>
-        <Button buttonLabel='Request Beta Access' />
-      </p>
-      <div className='mb-14'>
-        <Image
-          src='/image-hero-landscape.png'
-          width={343}
-          height={240}
-          alt='mobile phone'
-        />
-      </div>
-
+    <div className='flex flex-col desktop:flex-row '>
+      <section className='outline tablet:bg-[url(/image-hero-portrait.png)] bg-no-repeat bg-[length:281px_482px] bg-right-top desktop:grow'>
+        <h1
+          style={{ backgroundImage: "url(/pattern-curved-line-1.svg)" }}
+          className='inline-block pt-5 text-[2.375rem] tablet:text-[4rem] leading-[1.1578]  bg-no-repeat bg-right-top bg-[length:151px_19px]'
+        >
+          A <span className='font-bold'>super solution</span> <br />
+          for your <span className='font-bold'>business.</span>
+        </h1>
+        <p className='text-grey text-[1rem] leading-[1.75] tracking-[0.09px] mb-8'>
+          Our marketing and sales automation help you scale your outreach to get
+          more leads for your company.
+        </p>
+        <p>
+          <Button buttonLabel='Request Beta Access' />
+        </p>
+        <div className=' tablet:hidden'>
+          <Image
+            src='/image-hero-landscape.png'
+            width={343}
+            height={240}
+            alt='mobile phone'
+          />
+        </div>
+      </section>
       <Stats />
-    </section>
+    </div>
   );
 }
 
--- app/_components/Stat.js
+++ app/_components/Stat.js
@@ -3,7 +3,7 @@
 function Stat({ title, subtitle }) {
   return (
     <div className='text-center'>
-      <h3 className='font-bold text-[48px] leading-[56px] tracking-normal'>
+      <h3 className='font-bold text-[3rem] leading-[1.1667] tracking-normal'>
         {title}
       </h3>
       <p className='text-sm uppercase text-grey'>{subtitle}</p>
