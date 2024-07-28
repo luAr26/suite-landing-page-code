665eb4a12d114a458054c9cc04c36abbb3dc1478
Raul <raul_cosmin_26@yahoo.com>
2024-07-28 21:06:21 +0300

    Add social media icons to the footer.

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,15 +1,37 @@
-import Logo from "@/app/_components/Logo";
 import Link from "next/link";
+
+import Logo from "@/app/_components/Logo";
+import FacebookIcon from "@/app/_components/FacebookIcon";
+import TwitterIcon from "@/app/_components/TwitterIcon";
+import InstagramIcon from "@/app/_components/InstagramIcon";
 
 function Footer() {
   return (
-    <footer className="relative z-0 mt-28 flex items-center justify-between">
+    <footer className="relative z-0 mt-28 flex items-center justify-between bg-cream">
       <Logo />
       <p className="text-footer text-grey">&copy; Copyright - Suite</p>
-      <div>
-        <Link href="/">Facebook</Link>
-        <Link href="/">Twitter</Link>
-        <Link href="/">Instagram</Link>
+      <div className="flex items-center gap-7">
+        <Link
+          href="https://www.facebook.com/login/"
+          target="_blank"
+          className="social-link"
+        >
+          <FacebookIcon />
+        </Link>
+        <Link
+          href="https://x.com/?lang=en"
+          target="_blank"
+          className="social-link"
+        >
+          <TwitterIcon />
+        </Link>
+        <Link
+          href="https://www.instagram.com/"
+          target="_blank"
+          className="social-link"
+        >
+          <InstagramIcon />
+        </Link>
       </div>
     </footer>
   );
