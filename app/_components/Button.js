e370221f007f9fc298ee1eb48c55a6c2e88356b7
Raul <raul_cosmin_26@yahoo.com>
2024-07-29 22:11:46 +0300

    Refactor the button styles.

--- app/_components/Button.js
+++ app/_components/Button.js
@@ -5,8 +5,8 @@ import Link from "next/link";
 function Button({ buttonLabel }) {
   return (
     <Link
-      href='/'
-      className='inline-block px-[26px] py-[10px] font-bold leading-8 border-2 rounded-md text-dark-blue hover:bg-dark-blue hover:text-cream-white transition-colors duration-150'
+      href="/"
+      className="inline-block rounded-md border px-[1.362em] py-[0.786em] text-[0.875rem] font-bold leading-[1.71428] text-dark-blue"
     >
       {buttonLabel}
     </Link>
